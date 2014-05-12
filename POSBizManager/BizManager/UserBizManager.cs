e5f711df50c4ce59e975658f4c185fd9d33e8797
Burnishtech <tapas_nayak@burnish.co.in>
2014-05-12 23:43:43 +0530

    commit

---
+++ POSBizManager/BizManager/UserBizManager.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using POSDataManager.Entity;
+using POSDataManager.DataManager;
+
+namespace POSBizManager.BizManager
+{
+    public class UserBizManager
+    {
+        UserDataManager udm = new UserDataManager();
+         List<User> getAllUser()  
+        {
+            return udm.getAllUser();
+        }
+         public bool validateUser(string username, string password)
+        {
+            //+ Here you would implement code, which will get the validation for the given credentials
+            //+ from a database, a webservice or from somewhere else
+            //! Remember : ONLY for demonstration purposes I have used a local Collection
+
+            User validatedUser = udm.getAllUser().FirstOrDefault(user => user.UserName.Equals(username) && user.Password.Equals(password));
+            return validatedUser != null;
+        }
+    }
+}
---
+++ POSDtataManager/DataManager/UserDataManager.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using POSDataManager.Entity;
+using System.IO;
+
+namespace POSDataManager.DataManager
+{
+    public class UserDataManager
+    {
+        #region Fields
+
+        List<User> userList;
+		private readonly string userImagesPath = @"\Images";
+
+		#endregion // Fields
+        
+        public List<User> getAllUser()
+        {
+
+            this.userList = new List<User>()
+								 {
+									new User() { UserName="tapas", Password="nayak",
+													 ImageSourcePath = Path.Combine( userImagesPath, "DemoUser2.png") },
+									new User() { UserName="burnish", Password="burnish",
+													 ImageSourcePath = Path.Combine( userImagesPath, "DemoUser1.png") },
+								 };
+            return userList;
+        }
+    }
+}
