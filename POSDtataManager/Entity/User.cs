3aed4a31e7ea91a0c56df9fe5beaa944dad6433e
Burnishtech <tapas_nayak@burnish.co.in>
2014-05-14 23:03:09 +0530

    V0.1

---
+++ POSDtataManager/Entity/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace POSDataManager.Entity
+{
+    public class User
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string EMailAddress { get; set; }
+        public string ImageSourcePath { get; set; }
+    }
+}
